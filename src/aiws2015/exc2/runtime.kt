24078f3a039d498805af839c450272ef93f7d4d2
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2015-02-05 18:00:17 +0300

    Cleanup

--- src/aiws2015/exc2/runtime.kt
+++ src/aiws2015/exc2/runtime.kt
@@ -22,9 +22,35 @@ fun main(args: Array<String>) {
 }
 
 
-val MAX_INSTRUCTIONS = 1000
+
 
 fun execute(input: Int, instructions: List<Instruction>, debug: Boolean): Int {
+
+    val MAX_INSTRUCTIONS = 1000
+
+    fun eval(instruction: Instruction, s: State): State? {
+        return when (instruction) {
+            is INC -> when (instruction.v) {
+                'x' -> State(s.ps + 1, s.xv + 1, s.yv, s.zv)
+                'y' -> State(s.ps + 1, s.xv, s.yv + 1, s.zv)
+                'z' -> State(s.ps + 1, s.xv, s.yv, s.zv + 1)
+                else -> null;
+            }
+            is DEC -> when (instruction.v) {
+                'x' -> if (s.xv > 0) State(s.ps + 1, s.xv - 1, s.yv, s.zv) else null
+                'y' -> if (s.yv > 0) State(s.ps + 1, s.xv, s.yv - 1, s.zv) else null
+                'z' -> if (s.zv > 0) State(s.ps + 1, s.xv, s.yv, s.zv - 1) else null
+                else -> null;
+            }
+            is ZERO -> when (instruction.v) {
+                'x' -> if (s.xv == 0) State(instruction.p1, s.xv, s.yv, s.zv) else State(instruction.p2, s.xv, s.yv, s.zv)
+                'y' -> if (s.yv == 0) State(instruction.p1, s.xv, s.yv, s.zv) else State(instruction.p2, s.xv, s.yv, s.zv)
+                'z' -> if (s.zv == 0) State(instruction.p1, s.xv, s.yv, s.zv) else State(instruction.p2, s.xv, s.yv, s.zv)
+                else -> null;
+            }
+            else -> null
+        }
+    }
     // Init state contract
     var state = State(1, input, 0, 0)
 
@@ -50,26 +76,4 @@ fun execute(input: Int, instructions: List<Instruction>, debug: Boolean): Int {
     throw RuntimeException("NOT finished in in $MAX_INSTRUCTIONS steps, result: ${state.yv}")
 }
 
-private fun eval(instruction: Instruction, s: State): State? {
-    return when (instruction) {
-        is INC -> when (instruction.v) {
-            'x' -> State(s.ps + 1, s.xv + 1, s.yv, s.zv)
-            'y' -> State(s.ps + 1, s.xv, s.yv + 1, s.zv)
-            'z' -> State(s.ps + 1, s.xv, s.yv, s.zv + 1)
-            else -> null;
-        }
-        is DEC -> when (instruction.v) {
-            'x' -> if (s.xv > 0) State(s.ps + 1, s.xv - 1, s.yv, s.zv) else null
-            'y' -> if (s.yv > 0) State(s.ps + 1, s.xv, s.yv - 1, s.zv) else null
-            'z' -> if (s.zv > 0) State(s.ps + 1, s.xv, s.yv, s.zv - 1) else null
-            else -> null;
-        }
-        is ZERO -> when (instruction.v) {
-            'x' -> if (s.xv == 0) State(instruction.p1, s.xv, s.yv, s.zv) else State(instruction.p2, s.xv, s.yv, s.zv)
-            'y' -> if (s.yv == 0) State(instruction.p1, s.xv, s.yv, s.zv) else State(instruction.p2, s.xv, s.yv, s.zv)
-            'z' -> if (s.zv == 0) State(instruction.p1, s.xv, s.yv, s.zv) else State(instruction.p2, s.xv, s.yv, s.zv)
-            else -> null;
-        }
-        else -> null
-    }
-}
+
