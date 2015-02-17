7c723fde045b77d21300a3f4d8cf4865f8c4fa93
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2015-02-17 15:19:48 +0300

    Workaround for https://youtrack.jetbrains.com/issue/KT-6744

--- src/aiws2015/exc4/interval.kt
+++ src/aiws2015/exc4/interval.kt
@@ -16,15 +16,15 @@ open class Interval() : Lattice<Interval> {
     }
 
     data class NegInf(val b: Int) : Interval() {
-        override fun toString() = "[-Inf; $b]"
+        override fun toString() = "[-Inf; ${this.b}]"
     }
 
     data class PosInf(val a: Int) : Interval() {
-        override fun toString() = "[$a; +Inf]"
+        override fun toString() = "[${this.a}; +Inf]"
     }
 
     data class Range(val a: Int, val b: Int) : Interval() {
-        override fun toString() = "[$a; $b]"
+        override fun toString() = "[${this.a}; ${this.b}]"
     }
 
     override fun name(): String = "Interval domain"
@@ -41,9 +41,9 @@ open class Interval() : Lattice<Interval> {
         return when (this) {
             is NegInf -> other is NegInf && this.b <= other.b
             is Range -> when (other) {
-                is NegInf -> b <= other.b
-                is Range -> a >= other.a && this.b <= other.b
-                is PosInf -> a >= other.a
+                is NegInf -> this.b <= other.b
+                is Range -> this.a >= other.a && this.b <= other.b
+                is PosInf -> this.a >= other.a
                 else -> false
             }
             is PosInf -> other is PosInf && this.a >= other.a
@@ -61,40 +61,40 @@ open class Interval() : Lattice<Interval> {
             when (this) {
                 is Bottom -> Bottom
                 is Top -> Range(0, 0)
-                is NegInf -> if (b >= 0) Range(0, 0) else Bottom
-                is PosInf -> if (a <= 0) Range(0, 0) else Bottom
-                is Range -> if (a <= 0 && 0 <= b) Range(0, 0) else Bottom
+                is NegInf -> if (this.b >= 0) Range(0, 0) else Bottom
+                is PosInf -> if (this.a <= 0) Range(0, 0) else Bottom
+                is Range -> if (this.a <= 0 && 0 <= this.b) Range(0, 0) else Bottom
                 else -> throw RuntimeException()
             }
 
     override fun notzero(): Interval =
             when (this) {
                 is Range ->
-                    if (a == 0 && b == 0) Bottom
-                    else if (a == 0) Range(1, b)
-                    else if (b == 0) Range(a, -1)
+                    if (this.a == 0 && this.b == 0) Bottom
+                    else if (this.a == 0) Range(1, this.b)
+                    else if (this.b == 0) Range(this.a, -1)
                     else this
-                is NegInf -> if (b == 0) NegInf(-1) else this
-                is PosInf -> if (a == 0) PosInf(0) else this
+                is NegInf -> if (this.b == 0) NegInf(-1) else this
+                is PosInf -> if (this.a == 0) PosInf(0) else this
                 else -> this
             }
 
     override fun incr(): Interval =
             when (this) {
-                is Range -> Range(a + 1, b + 1)
-                is NegInf -> NegInf(b + 1)
-                is PosInf -> PosInf(a + 1)
+                is Range -> Range(this.a + 1, this.b + 1)
+                is NegInf -> NegInf(this.b + 1)
+                is PosInf -> PosInf(this.a + 1)
                 else -> this
             }
 
     override fun decr(): Interval = when (this) {
-        is Range -> Range(a - 1, b - 1)
-        is NegInf -> NegInf(b - 1)
-        is PosInf -> PosInf(a - 1)
+        is Range -> Range(this.a - 1, this.b - 1)
+        is NegInf -> NegInf(this.b - 1)
+        is PosInf -> PosInf(this.a - 1)
         else -> this
     }
 
-    fun normalize() : Interval = if (this is Range && a > b) Bottom else this
+    fun normalize() : Interval = if (this is Range && this.a > this.b) Bottom else this
 }
 
 
